64a83e09a56b26347705f7d83658c661aaa624a3
Naomi Cherotich <naomirono1454@gmail.com>
2023-02-09 16:39:14 +0300

    Add navbar, swiper and loadMore code

--- functions/public/src/js/script.js
+++ functions/public/src/js/script.js
@@ -61,3 +61,67 @@ app.get('/',function(req,res){
     }).listen(3002)
   
 console.log("server listening at port 3002");
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+ 
+  loop:true,
+});
+
+
+var swiper = new Swiper(".review-slider", {
+   spaceBetween: 20,
+   centeredSlides: true,
+   autoplay: {
+     delay: 7500,
+     disableOnInteraction: false,
+   },
+   loop:true,
+   breakpoints: {
+     0: {
+         slidesPerView: 1,
+     },
+     640: {
+       slidesPerView: 2,
+     },
+     768: {
+       slidesPerView: 2,
+     },
+     1024: {
+       slidesPerView: 3,
+     },
+   },
+ });
+
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   };
+   currentItem += 3;
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
+
